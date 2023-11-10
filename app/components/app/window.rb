bb95c7cec6ac7014317c88dee3b4603640731096
lpwanw <phuongtay2000@gmail.com>
2023-11-10 16:26:54 +0700

    Refactor Window componrnt

--- app/components/app/window.rb
+++ app/components/app/window.rb
@@ -3,8 +3,11 @@
 class App::Window < ViewComponent::Base
   renders_one :body
 
-  def initialize(id: nil)
+  def initialize(id: nil, title: nil, icon: "icons/setting.svg", js_controller: nil)
     @id = id
+    @title = title
+    @icon = icon
+    @js_controller = js_controller
     super
   end
 end
