45f31c5aaf498306155ab7a430a9f97e163ef1c1
lpwanw <phuongtay2000@gmail.com>
2023-11-11 14:41:00 +0700

    Task Model

---
+++ app/models/examples/task.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Examples::Task < ApplicationRecord
+  enum status: {
+    initialized: :initialized,
+    in_progress: :in_progress,
+    pending: :pending,
+    completed: :completed
+  }
+  validates :status, presence: true
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 255 }
+
+  validates :description, presence: true
+  validates :description, length: { maximum: 255 }
+end
