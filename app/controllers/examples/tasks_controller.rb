afd30b5edf7af5b37d012d770466669418784467
lpwanw <phuongtay2000@gmail.com>
2023-11-13 13:24:21 +0700

    DragDrop Sortable Js

--- app/controllers/examples/tasks_controller.rb
+++ app/controllers/examples/tasks_controller.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
 class Examples::TasksController < ApplicationController
+  before_action :load_task, only: %i[show edit update]
+
+  def show
+    redirect_to examples_sortable_index_path unless turbo_frame_request?
+  end
+
+  def edit
+    redirect_to examples_sortable_index_path unless turbo_frame_request?
+  end
+
   def update
-    task = Examples::Task.find_by(id: params[:id])
-    task.update status: params[:status]
+    @task.update task_update_params
+
+    render :show if params[:attribute]
+  end
+
+  private
+
+  def load_task
+    @task = Examples::Task.find_by(id: params[:id])
+  end
+
+  def task_update_params
+    params.require(:examples_task).permit(:name, :description, :status)
   end
 end
